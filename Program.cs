e7b1e9ce1dbed4022129ed0fb2ff59e03227084f
Vukkert <Vukkert@DESKTOP-FMS6LDH>
2022-12-02 22:17:03 +0700

    help

--- Program.cs
+++ Program.cs
@@ -2,19 +2,30 @@
 Random rnd = new Random(DateTime.Now.Millisecond);
 Console.WriteLine("Размер массива:");
 int[] arr = new int[int.Parse(Console.ReadLine())];
-int min = 0, max = 0;
-for (int i = 0; i < arr.Length; i++)
+Console.WriteLine("Размер минимальную границу массива:");
+int minrange = int.Parse(Console.ReadLine());
+Console.WriteLine("Размер максимальную границу массива:");
+int maxrange = int.Parse(Console.ReadLine());
+if(minrange < maxrange || arr.Length > 2)
 {
-    arr[i] = rnd.Next(-101, 100);
-    Console.Write(arr[i] + ", ");
-    if (arr[i] >= arr[max]) max = i;
-    if (arr[i] <= arr[min]) min = i;
+    int min = 0, max = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(minrange, maxrange);
+        Console.Write($"[{i}]" + arr[i] + ", ");
+        if (arr[i] >= arr[max]) max = i;
+        if (arr[i] <= arr[min]) min = i;
+    }
+    Console.WriteLine();
+    Console.WriteLine("самый большой элемент: " + arr[max] + " позиция:" + $" [{max}]");
+    Console.WriteLine("самый маленький элемент: " + arr[min] + " позиция" + $" [{min}]");
+    int swap = arr[min];
+    arr[min] = arr[max];
+    arr[max] = swap;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"[{i}]" + arr[i] + ", ");
+    }
 }
-Console.WriteLine();
-int swap = arr[min];
-arr[min] = arr[max];
-arr[max] = swap;
-foreach (var c in arr)
-{
-    Console.Write(c + ", ");
-}
+Console.WriteLine("че ты ввел.");
+
